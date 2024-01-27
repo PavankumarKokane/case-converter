3067b4809ff7ed581c92b73ca5cd3bc9d322e70c
PavankumarKokane <pavankokane2013@gmail.com>
2024-01-27 23:06:32 +0530

    old problem

--- src/components/UpperCase.js
+++ src/components/UpperCase.js
@@ -5,14 +5,12 @@ export default function UpperCase(props) {
     const [text, setText] = useState('');
     const handlerSetText = () => {
         setText(text.toUpperCase());
-        props.showAlert("Converted to UpperCase", "Success");
     }
     const handlerTextEnter = (event) => {
         setText(event.target.value);
     }
     const handlerCopy = () => {
         navigator.clipboard.writeText(text);
-        props.showAlert("Copied to Clipboard", "Success");
     }
   return (
     <div>
